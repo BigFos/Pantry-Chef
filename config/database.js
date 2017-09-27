c823ff67fbf9b4f3cc48931a975fbf090fb0ce85
Steven Foster Jr <Steven Foster Jr>
2017-09-26 21:30:04 -0700

    updated DB connection

--- config/database.js
+++ config/database.js
@@ -1,6 +1,8 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://localhost:27017/pantry_login' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url' : 'mongodb://heroku_mvq70x2m:pdakjus9rof64p1m01vm28c5vj@ds149724.mlab.com:49724/heroku_mvq70x2m'
+
+    // 'url' : 'mongodb://localhost:27017/pantry_login' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
